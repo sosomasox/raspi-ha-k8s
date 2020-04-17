cfe95f50a6c76afbbbc2f3365c8564ee36f94b8c
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-04-18 02:48:19 +0900

    Add scripts/recover.sh

---
+++ scripts/recover.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $(whoami) != "root" ]; then
+    echo 'You are not root.'
+    echo 'You need to be root authority to execute.'
+    exit 1
+fi
+
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
+chmod +x /home/pi/Build_HA_RasPi_K8s_Cluster/scripts/clean_network_if.sh
+/home/pi/Build_HA_RasPi_K8s_Cluster/scripts/clean_network_if.sh
+rm -rf /home/pi/.kube
+
+echo 
+echo 'recover done.'
+echo 'You should reboot your Raspberry Pi.'
+
+exit 0
